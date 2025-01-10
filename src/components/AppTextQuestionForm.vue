6f965dc847e9ef7a2317dd3a9eeb93240e1067e7
Ignacio Guerra <iguerra.dev@gmail.com>
2025-01-10 20:06:30 -0300

    Remove multiple choice questions

--- src/components/AppTextQuestionForm.vue
+++ src/components/AppTextQuestionForm.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
-import type { SurveyMultipleChoiceQuestion } from '@/models/SurveyQuestion';
-import type { SurveyQuestionOption } from '@/models/SurveyQuestionOption';
+import AppFormTextField from './AppFormTextField.vue';
 
 defineProps<{
   questionIndex: number;
 }>();
 const displayTextModel = defineModel<string>('displayText');
-const optionsModel = defineModel<SurveyQuestionOption[]>('options');
 const emits = defineEmits(['remove']);
 
 const removeQuestion = () => {
@@ -16,11 +14,6 @@ const removeQuestion = () => {
 <template>
   <div class="p-2 border border-gray-800 rounded-md">
     <AppFormTextField v-model="displayTextModel" :labelText="`Question ${questionIndex}`" />
-    <div class="grid grid-cols-2 gap-4">
-      <!-- <AppFormTextField v-for="option in question.options" :key="option.id" v-model="option" -->
-      <!-- labelText="Option 1" /> -->
-      <button type="button" class="px-4 py-2 bg-gray-800 text-white rounded-md">Add Option</button>
-    </div>
     <button type="button" class="px-4 py-2 bg-red-500 text-white rounded-md" @click="removeQuestion()">Remove
       Question</button>
   </div>
